14f6d99d1491035da9b3c2920d602933a7b922ae
Graf_D <indiyskiy@gmail.com>
2023-12-16 18:57:51 +0100

    still trying to run on tomcat

--- src/main/kotlin/com/arkhamusserver/arkhamus/Application.kt
+++ src/main/kotlin/com/arkhamusserver/arkhamus/Application.kt
@@ -2,16 +2,12 @@ package com.arkhamusserver.arkhamus
 
 import com.arkhamusserver.arkhamus.config.JwtProperties
 import com.arkhamusserver.arkhamus.model.dataaccess.UserAccountRepository
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean
-import org.springframework.security.crypto.password.PasswordEncoder
 
 
 @SpringBootApplication
--- src/main/kotlin/com/arkhamusserver/arkhamus/view/controller/GameController.kt
+++ src/main/kotlin/com/arkhamusserver/arkhamus/view/controller/GameController.kt
@@ -4,7 +4,7 @@ import com.arkhamusserver.arkhamus.logic.GameLogic
 import com.arkhamusserver.arkhamus.model.database.entity.GameSession
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.ResponseEntity
 import org.springframework.security.access.annotation.Secured
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
