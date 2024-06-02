66f0fa753d4ccfdc9da836844c91f64687ee8613
Graf_D <indiyskiy@gmail.com>
2024-06-02 21:21:38 +0200

    remove NettyExceptionHandler

--- src/main/kotlin/com/arkhamusserver/arkhamus/logic/ingame/loop/netty/exception/handler/NettyExceptionHandler.kt
+++
@@ -1,8 +0,0 @@
-package com.arkhamusserver.arkhamus.logic.ingame.loop.netty.exception.handler
-
-import com.arkhamusserver.arkhamus.view.dto.netty.response.NettyResponseMessage
-
-interface NettyExceptionHandler {
-    fun accept(exception: Exception): Boolean
-    fun parse(exception: Exception): NettyResponseMessage
-}
