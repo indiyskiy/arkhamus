44a7070da4cbfb0e736ebfb4ff1dd6cf61e68599
Graf_D <indiyskiy@gmail.com>
2024-08-07 14:29:34 +0200

    fix a bit redis config

--- src/main/kotlin/com/arkhamusserver/arkhamus/config/redis/RedisConfig.kt
+++ src/main/kotlin/com/arkhamusserver/arkhamus/config/redis/RedisConfig.kt
@@ -2,6 +2,7 @@ package com.arkhamusserver.arkhamus.config.redis
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 
@@ -9,13 +10,13 @@ import org.springframework.data.redis.core.RedisTemplate
 class RedisConfig {
     @Bean
     fun jedisConnectionFactory(): JedisConnectionFactory {
-        return JedisConnectionFactory()
+        return JedisConnectionFactory(RedisStandaloneConfiguration("localhost", 6379))
     }
 
     @Bean
-    fun redisTemplate(): RedisTemplate<String, Any> {
+    fun redisTemplate(connectionFactory: JedisConnectionFactory): RedisTemplate<String, Any> {
         val template: RedisTemplate<String, Any> = RedisTemplate()
-        template.connectionFactory = jedisConnectionFactory()
+        template.connectionFactory = connectionFactory
         return template
     }
 }
